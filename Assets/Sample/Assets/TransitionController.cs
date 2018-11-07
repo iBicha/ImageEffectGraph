0995a1e3120126586e81cac2f6a31c0c7eeb9500
Brahim Hadriche <brahim.hadriche@gmail.com>
2018-11-07 15:12:53 +0100

    Added a simple script to control the transition effect

---
+++ Assets/Sample/Assets/TransitionController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionController : MonoBehaviour
+{
+    public Material material;
+    public float speed = 1f;
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Transition"))
+        {
+            StartTransition();
+        }
+    }
+
+    void StartTransition()
+    {
+        StopAllCoroutines();
+        var startPosition = material.GetFloat("_Progress");
+        var direction = startPosition > 0.5 ? -1f : 1f;
+        StartCoroutine(TransitionRoutine(startPosition, direction));
+    }
+
+
+    IEnumerator TransitionRoutine(float startPosition, float direction)
+    {
+        while (direction > 0 && !Mathf.Approximately(startPosition, 1) ||
+               direction < 0 && !Mathf.Approximately(startPosition, 0))
+        {
+            startPosition = Mathf.Clamp01(startPosition + Time.deltaTime * direction * speed);
+            material.SetFloat("_Progress", startPosition);
+            yield return null;
+        }
+    }
+}
