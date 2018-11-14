2c334270f964c0fbf3e44180e33ef2df0a6cb101
Brahim Hadriche <brahim.hadriche@gmail.com>
2018-11-14 10:21:27 +0100

    Added showcase script to cycle through effect materials

---
+++ Assets/Sample/Scripts/CycleThroughEffects.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ImageEffectGraph.PostProcessing;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace ImageEffectGraph.Demo
+{
+    public class CycleThroughEffects : MonoBehaviour
+    {
+        public float cycleInterval = 1f;
+        public Material[] materials;
+
+        private RenderWithMaterial settings;
+        private int effectIndex = -1;
+
+        private TransitionController transitionController;
+        private GUIStyle styleLabel;
+        private GUIStyle styleButton;
+
+        void Start()
+        {
+            var volume = GetComponent<PostProcessVolume>();
+            if (!volume.profile.HasSettings<RenderWithMaterial>())
+            {
+                Debug.LogWarning("RenderWithMaterial effect not found");
+                Destroy(this);
+                return;
+            }
+
+            settings = volume.profile.GetSetting<RenderWithMaterial>();
+            transitionController = GetComponent<TransitionController>();
+
+            InvokeRepeating("SetNextEffect", 0, cycleInterval);
+        }
+
+        private void SetNextEffect()
+        {
+            effectIndex = (effectIndex + 1) % materials.Length;
+            settings.material.value = materials[effectIndex];
+
+            if (materials[effectIndex] == transitionController.material)
+            {
+                transitionController.StartTransition();
+            }
+        }
+
+        private void OnGUI()
+        {
+            //This is not good, but whatever.
+            if (styleLabel == null || styleButton == null)
+            {
+                styleLabel = new GUIStyle(GUI.skin.box) {fontSize = 36};
+                styleButton = new GUIStyle(GUI.skin.button) {fontSize = 36};
+            }
+
+            GUILayout.BeginHorizontal();
+            if (effectIndex != -1)
+            {
+                GUILayout.Label(materials[effectIndex].name, styleLabel);
+            }
+
+            if (GUILayout.Button("Next effect", styleButton))
+            {
+                CancelInvoke();
+                SetNextEffect();
+                InvokeRepeating("SetNextEffect", cycleInterval, cycleInterval);
+            }
+
+            GUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/Sample/Scripts/TransitionController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ImageEffectGraph.Demo
+{
+    public class TransitionController : MonoBehaviour
+    {
+        public Material material;
+        public float speed = 1f;
+    
+        public void StartTransition()
+        {
+            StopAllCoroutines();
+            var startPosition = material.GetFloat("_Progress");
+            var direction = startPosition > 0.5 ? -1f : 1f;
+            StartCoroutine(TransitionRoutine(startPosition, direction));
+        }
+
+
+        IEnumerator TransitionRoutine(float startPosition, float direction)
+        {
+            while (direction > 0 && !Mathf.Approximately(startPosition, 1) ||
+                   direction < 0 && !Mathf.Approximately(startPosition, 0))
+            {
+                startPosition = Mathf.Clamp01(startPosition + Time.deltaTime * direction * speed);
+                material.SetFloat("_Progress", startPosition);
+                yield return null;
+            }
+        }
+    }
+}
